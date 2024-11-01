47e4bfbd8f233291766c7d6cbc081a8160f7919c
jal9o3 <jeromeardiente.loria@bicol-u.edu.ph>
2024-11-01 16:59:13 +0800

    defined global constants

---
+++ generals.py
@@ -0,0 +1,23 @@
+# Global constants for Game of the Generals
+ROWS = 8
+COLUMNS = 9
+
+# Power Rankings of Pieces
+BLANK = 0 # Unoccupied Square
+FLAG = 1 # Philippine Flag
+PRIVATE = 2 # One Chevron
+SERGEANT = 3 # Three Chevrons
+SECOND_LIEUTENANT = 4 # One Magdalo Triangle
+FIRST_LIEUTENANT = 5 # Two Magdalo Triangles
+CAPTAIN = 6 # Three Magdalo Triangles
+MAJOR = 7 # One Magdalo Seven-Ray Sun
+LIEUTENANT_COLONEL = 8 # Two Magdalo Seven-Ray Suns
+COLONEL = 9 # Three Magdalo Seven-Ray Suns
+BRIGADIER_GENERAL = 10 # One Star
+MAJOR_GENERAL = 11 # Two Stars
+LIEUTENANT_GENERAL = 12 # Three Stars
+GENERAL = 13 # Four Stars
+GENERAL_OF_THE_ARMY = 14 # Five Stars
+SPY = 15 # Two Prying Eyes
+# Red pieces will be denoted 16 (FLAG) to 30 (SPY)
+UNKNOWN = 31 # Placeholder for unidentified enemy pieces
