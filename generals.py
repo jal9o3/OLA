73d8d14aa7671cb3e19539cd01c86308ee3daba5
jal9o3 <jeromeardiente.loria@bicol-u.edu.ph>
2024-11-01 18:03:48 +0800

    formation placing in main function

--- generals.py
+++ generals.py
@@ -25,3 +25,69 @@ UNKNOWN = 31 # Placeholder for unidentified enemy pieces
 # Designations of players
 BLUE = 1 # Moves first
 RED = 2
+
+def main():
+    # Board for arbiter
+    board = [[BLANK for _ in range(COLUMNS)] for _ in range(ROWS)]
+    
+    # Boards for both player POVs
+    blue_board = [[BLANK for _ in range(COLUMNS)] for _ in range(ROWS)]
+    red_board = [[BLANK for _ in range(COLUMNS)] for _ in range(ROWS)]
+    
+    # Initial formations span three rows
+    blue_formation = [BLANK for _ in range(COLUMNS) for _ in range(ROWS)]
+    red_formation = [BLANK for _ in range(COLUMNS) for _ in range(ROWS)]
+
+    # formation_temp = input("BLUE formation: ")
+    formation_temp = "1 15 15 2 2 2 2 2 2 3 4 5 6 7 8 9 10 11 12 13 14 0 0 0 0 0 0"
+    # Preprocess input
+    for i, p in enumerate(formation_temp.split(" ")):
+        blue_formation[i] = int(p)
+
+    # Place pieces on blue board
+    i = 0
+    for row in range(ROWS-3, ROWS):
+        for column in range(COLUMNS):
+            if i < len(blue_formation):
+                blue_board[row][column] = blue_formation[i]
+                i += 1
+
+    print_matrix(blue_board)
+
+    # formation_temp = input("RED formation: ")
+    formation_temp = "1 15 0 2 2 2 2 2 2 3 4 5 6 7 0 9 10 11 12 13 14 0 0 8 0 15 0"
+    # Preprocess input
+    for i, p in enumerate(formation_temp.split(" ")):
+        if int(p) != BLANK:
+            red_formation[i] = int(p) + SPY # Red pieces range from 15 to 30
+
+    # Place pieces on red board
+    i = 0
+    for row in range(ROWS-3, ROWS):
+        for column in range(COLUMNS):
+            if i < len(red_formation):
+                red_board[row][column] = red_formation[i]
+                i += 1
+    # Flip the matrix
+    # Flip the matrix upside down 
+    red_board = red_board[::-1] 
+    # Flip each row left to right 
+    red_board = [row[::-1] for row in red_board]
+
+    print_matrix(red_board)
+
+    # Perform matrix addition 
+    board = [[blue_board[i][j] + red_board[i][j] for j in range(COLUMNS)] for i in range(len(board))]
+
+    print_matrix(board)
+
+def print_matrix(matrix):
+    print()
+    for row in matrix: 
+        for elem in row: 
+            print(f"{elem:2}", end=' ') 
+        print()
+    print()       
+
+if __name__ == "__main__":
+    main()
