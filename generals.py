2ffa6ba4594a3d1688ec9e7af563e7d42ccc0f2b
jal9o3 <jeromeardiente.loria@bicol-u.edu.ph>
2024-11-04 13:28:06 +0800

    is_terminal function (untested)

--- generals.py
+++ generals.py
@@ -26,9 +26,15 @@ UNKNOWN = 31 # Placeholder for unidentified enemy pieces
 BLUE = 1 # Moves first
 RED = 2
 
+# Designations of the annotation indices
+CURRENT_PLAYER = 0
+WAITING_BLUE_FLAG = 1 # If blue flag reaches enemy base with an adjacent enemy
+WAITING_RED_FLAG = 2 # Same for the red flag
+
 def main():
     # Board for arbiter
     board = [[BLANK for _ in range(COLUMNS)] for _ in range(ROWS)]
+    annotation = [BLUE, 0, 0]
     
     # Boards for both player POVs
     blue_board = [[BLANK for _ in range(COLUMNS)] for _ in range(ROWS)]
@@ -81,17 +87,55 @@ def main():
     board = [[blue_board[i][j] + red_board[i][j] for j in range(COLUMNS)] for i in range(len(board))]
 
 
-    # Flip the board matrix:
-    board = board[::-1]
-    board = [row[::-1] for row in board]
+    # Flip the board matrix for the standard POV (blue on the bottom side):
+    standard_pov = board[::-1]
+    standard_pov = [row[::-1] for row in standard_pov] # flip rows
     
     print_matrix(board)
 
-def is_terminal(board):
+    print_matrix(standard_pov)
+
+def is_terminal(board, annotation):
     # If either of the flags have been captured
     if FLAG not in board or SPY + FLAG not in board:
         return True
 
+    # Procedure for checking adjacent enemy pieces in waiting flags
+    def has_adjacent(flag_col):
+        # If not at the left or rightmost edge of the board
+        if flag_col != 0 and flag_col != COLUMNS - 1:
+            # Check both squares to the left and right
+            if not board[-1][flag_col - 1] and not board[-1][flag_col + 1]:
+                return True
+        elif flag_col == 0 and not board[-1][flag_col + 1]:
+            # If flag is at the first column
+            # and the square next to it is empty
+            return True
+        elif flag_col == COLUMNS - 1 and not board[-1][flag_col - 1]:
+            # If flag is at the last column
+            # and the square before it is empty
+            return True
+    
+    # If the blue flag is on the other side of the board
+    if FLAG in board[-1]:
+        # If flag has already survived a turn
+        if annotation[WAITING_BLUE_FLAG]:
+            return True
+        else:
+            flag_col = board[-1].index(FLAG) # Get the flag's column number
+            return has_adjacent(flag_col)
+
+    # Do the same checking for the red flag
+    if SPY + FLAG in board[0]:
+        if annotation[WAITING_RED_FLAG]:
+            return True
+        else:
+            flag_col = board[0].index(FLAG)
+            return has_adjacent(flag_col)
+
+    # If none of the checks have been passed, it is not a terminal state
+    return False
+            
 def print_matrix(board):
     print()
     for row in board: 
