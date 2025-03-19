19c055a0759ab3b2b88576167a477455a226ad68
jal9o3 <jeromeardiente.loria@bicol-u.edu.ph>
2025-03-19 23:09:01 +0800

    added selection between match simulation and data generation in main.py

--- main.py
+++ main.py
@@ -16,37 +16,40 @@ def main():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
-    # Sample random formations
-    # blue_formation = list(
-    #     Player.get_sensible_random_formation(
-    #         piece_list=Ranking.SORTED_FORMATION)
-    # )
-    # red_formation = list(
-    #     Player.get_sensible_random_formation(
-    #         piece_list=Ranking.SORTED_FORMATION)
-    # )
+    choice = input("1 - Simulate a match\n2 - Generate training data\n>> ")
 
-    # match_simulator = MatchSimulator(formations=[blue_formation, red_formation],
-    #                                  controllers=[
-    #                                      Controller.RANDOM, Controller.RANDOM],
-    #                                  save_data=False,
-    #                                  pov=POV.RED)
-    # match_simulator.start()
+    if choice == "1":
+        print("Selected match simulation.")
+        # Sample random formations
+        blue_formation = list(
+            Player.get_sensible_random_formation(
+                piece_list=Ranking.SORTED_FORMATION)
+        )
+        red_formation = list(
+            Player.get_sensible_random_formation(
+                piece_list=Ranking.SORTED_FORMATION)
+        )
 
+        match_simulator = MatchSimulator(formations=[blue_formation, red_formation],
+                                         controllers=[
+                                             Controller.RANDOM, Controller.RANDOM],
+                                         save_data=False,
+                                         pov=POV.RED)
+        match_simulator.start()
 
-    # TRAINING START
+    elif choice == "2":
+        print("Selected training data generation.")
 
-    # simulator = CFRTrainingSimulator(formations=[None, None],
-    #                                  controllers=None, save_data=False,
-    #                                  pov=POV.WORLD)
+        # simulator = CFRTrainingSimulator(formations=[None, None],
+        #                                  controllers=None, save_data=False,
+        #                                  pov=POV.WORLD)
 
-    simulator = AlphaBetaTrainingSimulator(formations=[None, None],
-                                           controllers=None, save_data=False,
-                                           pov=POV.WORLD)
+        simulator = AlphaBetaTrainingSimulator(formations=[None, None],
+                                               controllers=None, save_data=False,
+                                               pov=POV.WORLD)
 
-    simulator.start(target=1)
+        simulator.start(target=1)
 
-    # TRAINING END
 
 if __name__ == "__main__":
     main()
