99d30204a4929e2e80088cd07c0035a1bd230d1c
jal9o3 <jeromeardiente.loria@bicol-u.edu.ph>
2025-04-23 02:33:44 +0800

    set target to 20k again

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def main():
         simulator = CFRTrainingSimulator(formations=[None, None],
                                          controllers=None, save_data=False,
                                          pov=POV.WORLD)
-        simulator.start(target=1)
+        simulator.start(target=20000)
 
 
 if __name__ == "__main__":
